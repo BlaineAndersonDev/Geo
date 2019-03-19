9f77feb3f6429614de41b243d4fe9dfa0550a9d5
Blaine Anderson <BlaineAndersonDev@gmail.com>
2019-03-19 12:53:15 -0700

    Test

--- knexfile.js
+++ knexfile.js
@@ -7,22 +7,6 @@ require('dotenv').config();
 
 module.exports = {
 
-  development: {
-    client: 'pg',
-    connection: {
-      host : '127.0.0.1',
-      user : process.env.REACT_APP_SERVER_KNEX_DEV_DB_USERNAME,
-      password : process.env.REACT_APP_SERVER_KNEX_DEV_DB_PASSWORD,
-      database : process.env.REACT_APP_SERVER_KNEX_DEV_DB_NAME,
-      charset: 'utf8'
-    },
-    migrations: {
-      directory: __dirname + '/knex/migrations',
-    },
-    seeds: {
-      directory: __dirname + '/knex/seeds'
-    }
-  },
 
   production: {
     client: 'pg',
